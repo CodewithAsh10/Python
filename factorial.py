b870701e490c22e62cc5686b3faba3851ff561e1
Arsh Bakshi <arsh.cp10@gmail.com>
2024-12-25 19:34:30 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,7 @@
 def factorial(n):
-    result = 1
-    for i in range(1,n+1):
-        result *= i 
-    return result
-
-num = int(input("Enter the number: "))
+    if n==0 or n==1:
+        return 1 
+    else:
+        return n*factorial(n-1)
+num = int(input("Enter the no: "))
 print(f"Factorial of {num} is {factorial(num)}")
- 
